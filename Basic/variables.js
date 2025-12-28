fdb28110462e990c9db3bd2685e3ee63580228bc
ravijuneja-coder <64406858+ravijuneja-coder@users.noreply.github.com>
2025-12-28 11:12:27 +0000

    variable

---
+++ Basic/variables.js
@@ -0,0 +1,12 @@
+const accountID = 123321. //const can not chaneg
+let accoutEmail = "Ravi@ravi.com"
+var accountPassword = "123098"
+accountCity = "New Delhi"
+
+accountEmail = "juneja@ravi.com"
+accountPassword = "232323"
+accountCity = "Noida"
+
+console.log(accountEmail);
+
+console.table([accoutEmail, accountPassword, accountCity])  //console table use for print multiple variable in table format
